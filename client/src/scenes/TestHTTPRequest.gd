3ad3c3555de770c3870e72f428eb285690e69d05
tylerhand1 <tyha7462@colorado.edu>
2024-05-03 10:19:52 -0600

    Added a label object to scene to show output within game Added timeout to TestHTTPRequest

--- client/src/scenes/TestHTTPRequest.gd
+++ client/src/scenes/TestHTTPRequest.gd
@@ -1,8 +1,14 @@
 extends HTTPRequest
 
+var label
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	label = get_parent().get_node("Label")
+	
 	var http_request = HTTPRequest.new()
+	# set a timeout of 1 second
+	http_request.set_timeout(1.0)
 	add_child(http_request)
 	http_request.connect("request_completed", self, "_http_request_completed")
 	
@@ -11,6 +17,11 @@ func _ready():
 		push_error("An error occurred in the HTTP request")
 
 func _http_request_completed(result, response_code, headers, body):
-	var response = parse_json(body.get_string_from_utf8())
+	if len(body) == 0:
+		print("No response")
+		label.text = "No response"
+		return
 	
+	var response = parse_json(body.get_string_from_utf8())
 	print(response['test'])
+	label.text = response['test']
