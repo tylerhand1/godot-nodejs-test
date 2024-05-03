53c9c85900b56dfa51b092cd6aad271f53059d7b
tylerhand1 <tyha7462@colorado.edu>
2024-05-02 19:35:00 -0600

    Created basic express server that serves GET /

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
